d148260b2f004093ed9361e0177b176c45c0d5c3
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2022-08-15 17:28:33 +0300

    fix typo

--- contracts/DelayValut.sol
+++ contracts/DelayValut.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "poolz-helper-v2/contracts/ERC20Helper.sol";
 import "poolz-helper-v2/contracts/ETHHelper.sol";
-import "poolz-helper-v2/contracts/Interfaces/ILockedDeal.sol";
+import "poolz-helper-v2/contracts/interfaces/ILockedDeal.sol";
 import "./ValutManageable.sol";
 
 /// @title main DelayValut settings
