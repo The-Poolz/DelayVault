3483b6b7dbd829a52f85b465a6b3c2f84c7b9acf
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2022-08-26 15:28:42 +0300

    fix lockperiod bug

--- contracts/DelayVault.sol
+++ contracts/DelayVault.sol
@@ -59,6 +59,7 @@ contract DelayVault is VaultData, ERC20Helper {
         uint64 finishTime = uint64(block.timestamp) + vault.LockPeriod;
         uint256 lockAmount = vault.Amount;
         vault.Amount = 0;
+        vault.LockPeriod = 0;
         ApproveAllowanceERC20(_token, LockedDealAddress, vault.Amount);
         ILockedDeal(LockedDealAddress).CreateNewPool(
             _token,
