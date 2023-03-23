6455af5846ab6b2a5361fc4a8a4ac331e6ae551b
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2023-03-23 19:29:16 +0200

    added reverts tests for view functions

--- test/3_Data.js
+++ test/3_Data.js
@@ -85,6 +85,19 @@ contract("Delay vault data", (accounts) => {
         )
     })
 
+    it("should revert invalid indices", async () => {
+        await truffleAssert.reverts(
+            instance.GetAllUsersDataRange(tokens[0].address, 100, 0),
+            "_from index can't be greater than _to"
+        )
+        await truffleAssert.reverts(
+            instance.GetAllMyTokensRange(tokens[0].address, 100, 0),
+            "_from index can't be greater than _to"
+        )
+        await truffleAssert.reverts(instance.GetAllUsersDataRange(tokens[0].address, 5, 10), "index out of range")
+        await truffleAssert.reverts(instance.GetAllMyTokensRange(tokens[0].address, 5, 10), "index out of range")
+    })
+
     it("should get my token addresses", async () => {
         const amounts = [amount, amount * 2, amount * 3]
         const finishDelays = [week, week * 2, week * 3]
