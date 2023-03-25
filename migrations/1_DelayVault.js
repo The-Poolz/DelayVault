4fe857f91cd869575c8052d3d54f3cb9dc23adc1
Stan Goldin <salty87@gmail.com>
2023-03-25 20:06:10 +0300

    add soliditycoverage (for real now)

--- migrations/1_DelayVault.js
+++ migrations/1_DelayVault.js
@@ -9,7 +9,7 @@ module.exports = async function (deployer, network, accounts) {
   await delayVaultInstance.setGovernerContract(deployerAddress);
 
   // Transfer ownership to the multisig wallet (only for non-testnet and non-local networks)
-  if (network !== "test" && network !== "development") {
+  if (network !== "test" && network !== "development" && network !== "soliditycoverage") {
     if (multisigWalletAddress == "0x1234567890123456789012345678901234567890") throw "replace with the actual multisig wallet address!"
     await delayVaultInstance.transferOwnership(multisigWalletAddress);
   }
