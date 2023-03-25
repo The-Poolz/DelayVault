92ca7228b22af53c80b37b15352b53701ad8fd00
Stan Goldin <48094744+Lomet@users.noreply.github.com>
2023-03-25 20:41:54 +0300

    Issue #133 (#140)
    * issue #133
    
    * fix test
    
    * more test fix
    
    * fix test
    
    * Update DelayEvents.sol

--- contracts/DelayManageable.sol
+++ contracts/DelayManageable.sol
@@ -90,7 +90,7 @@ contract DelayManageable is
     }
 
     /// @dev redemption of approved ERC-20 tokens from the contract
-    function BuyBackTokens(
+    function redeemTokensFromVault(
         address _token,
         address _owner,
         uint256 _amount
@@ -108,6 +108,6 @@ contract DelayManageable is
         if (vault.Amount == 0)
             vault.FinishDelay = vault.CliffDelay = vault.StartDelay = 0; // if Amount is zero, refresh vault values
         TransferToken(_token, msg.sender, _amount);
-        emit BoughtBackTokens(_token, _amount, vault.Amount);
+        emit RedeemedTokens(_token, _amount, vault.Amount);
     }
 }
